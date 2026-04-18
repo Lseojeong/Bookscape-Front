c3884a1c56bc17c298ac785925c39edb649098b8
Leeseojeong <sjung0314@gmail.com>
2026-04-18 15:30:26 +0900

    🎉 Init: 프로젝트 첫 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+// TODO: metadata 수정하기
+export const metadata: Metadata = {
+  title: "북스케이프: bookscape",
+  description:
+    "체험을 만들고, 찾고, 예약까지 한 번에 북스케이프에서 경험해보세요!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
