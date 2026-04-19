af024aab4f4730ccca7ec5a06db00a6b0ca78104
Leeseojeong <sjung0314@gmail.com>
2026-04-20 05:17:27 +0900

    🔧 Chore: 라우트 폴더구조 세팅

---
+++ src/app/(private)/activity/new/page.tsx
@@ -0,0 +1,3 @@
+export default function ActivityNewPage() {
+  return <div>체험 등록 페이지입니다.</div>;
+}
---
+++ src/app/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function ErrorPage() {
+  return <div>500 에러 페이지입니다.</div>;
+}
