4c3a42733272f0db1c085fce403aa00647a65829
Leeseojeong <sjung0314@gmail.com>
2026-04-20 22:41:22 +0900

    🔧 Chore: 이미지 파일 추가

---
+++ src/shared/assets/images/index.ts
@@ -0,0 +1,15 @@
+import DefaultProfileImage from './img-default-profile.svg';
+import EmptyImage from './img-empty.svg';
+import SkyImage from './img-sky.png';
+import SpeechBubbleImage from './img-speech-bubble.svg';
+import WarningImage from './img-warning.svg';
+
+const images = {
+  DefaultProfileImage,
+  EmptyImage,
+  SkyImage,
+  SpeechBubbleImage,
+  WarningImage,
+};
+
+export default images;
