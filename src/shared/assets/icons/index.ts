374fbdb9d393c8340e6f81ac6e2927a105eac884
Leeseojeong <sjung0314@gmail.com>
2026-04-20 22:42:11 +0900

    🔧 Chore: 아이콘 파일 추가

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,69 @@
+import ActivityIcon from './ic-activity.svg';
+import ArrowLeftIcon from './ic-arrow-left.svg';
+import ArrowRightIcon from './ic-arrow-right.svg';
+import ArtIcon from './ic-art.svg';
+import BackIcon from './ic-back.svg';
+import CaretDownIcon from './ic-caret-down.svg';
+import CaretLeftIcon from './ic-caret-left.svg';
+import CaretRightIcon from './ic-caret-right.svg';
+import ChevronLeftIcon from './ic-chevron-left.svg';
+import ChevronRightIcon from './ic-chevron-right.svg';
+import DeleteIcon from './ic-delete.svg';
+import EditIcon from './ic-edit.svg';
+import EyeOffIcon from './ic-eye-off.svg';
+import EyeOnIcon from './ic-eye-on.svg';
+import FoodIcon from './ic-food.svg';
+import GithubIcon from './ic-github.svg';
+import InfoIcon from './ic-info.svg';
+import KakaoIcon from './ic-kakao.svg';
+import LinkIcon from './ic-link.svg';
+import LocationIcon from './ic-location.svg';
+import LogoutIcon from './ic-logout.svg';
+import MenuIcon from './ic-menu.svg';
+import MinusIcon from './ic-minus.svg';
+import NotificationIcon from './ic-notification.svg';
+import PlusIcon from './ic-plus.svg';
+import ReservationListIcon from './ic-reservation-list.svg';
+import ReservationStatusIcon from './ic-reservation-status.svg';
+import SearchIcon from './ic-search.svg';
+import SightseeingIcon from './ic-sightseeing.svg';
+import StarIcon from './ic-star.svg';
+import TourIcon from './ic-tour.svg';
+import WellbeingIcon from './ic-wellbeing.svg';
+
+const icons = {
+  ActivityIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArtIcon,
+  BackIcon,
+  CaretDownIcon,
+  CaretLeftIcon,
+  CaretRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DeleteIcon,
+  EditIcon,
+  EyeOffIcon,
+  EyeOnIcon,
+  FoodIcon,
+  GithubIcon,
+  InfoIcon,
+  KakaoIcon,
+  LinkIcon,
+  LocationIcon,
+  LogoutIcon,
+  MenuIcon,
+  MinusIcon,
+  NotificationIcon,
+  PlusIcon,
+  ReservationListIcon,
+  ReservationStatusIcon,
+  SearchIcon,
+  SightseeingIcon,
+  StarIcon,
+  TourIcon,
+  WellbeingIcon,
+};
+
+export default icons;
