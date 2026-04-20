9923c0dde4f6547b7793a1646d1cf65ed35617df
Leeseojeong <sjung0314@gmail.com>
2026-04-20 22:54:32 +0900

    🔧 Chore: svgr 설정

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,8 +1,8 @@
-import DefaultProfileImage from './img-default-profile.svg';
-import EmptyImage from './img-empty.svg';
+import DefaultProfileImage from './img-default-profile.svg?url';
+import EmptyImage from './img-empty.svg?url';
 import SkyImage from './img-sky.png';
-import SpeechBubbleImage from './img-speech-bubble.svg';
-import WarningImage from './img-warning.svg';
+import SpeechBubbleImage from './img-speech-bubble.svg?url';
+import WarningImage from './img-warning.svg?url';
 
 const images = {
   DefaultProfileImage,
