2fbb04f205a49712f0b89ac21a7b920d29dd59bc
YEO Sugyeong <ssyeo1204@gmail.com>
2026-04-20 23:51:46 +0900

    🔧 Chore: Tailwind 초기 세팅 및 폰트, 컬러 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '@/shared/styles/globals.css';
+import localFont from 'next/font/local';
 
 // TODO: metadata 수정하기
 export const metadata: Metadata = {
@@ -7,13 +8,21 @@ export const metadata: Metadata = {
   description: '체험을 만들고, 찾고, 예약까지 한 번에 북스케이프에서 경험해보세요!',
 };
 
+// 폰트 파일 로드 + CSS 변수 등록
+const pretendard = localFont({
+  src: '../shared/assets/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${pretendard.variable}`}>
       <body>{children}</body>
     </html>
   );
