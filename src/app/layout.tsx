cee032d2d8dd1e6ef23ac5566ebc384f073f6aed
Leeseojeong <sjung0314@gmail.com>
2026-04-21 23:21:14 +0900

    📝 Docs: 메타태그 수정 주석 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import QueryProvider from '@/shared/providers/QueryProvider';
 import '@/shared/styles/globals.css';
 
+//TODO: 메타태그 수정하기
 export const metadata: Metadata = {
   title: '북스케이프: bookscape',
   description: '체험을 만들고, 찾고, 예약까지 한 번에 북스케이프에서 경험해보세요!',
