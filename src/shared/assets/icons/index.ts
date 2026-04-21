899b490a0215e9d5e2e6db85e91d6ed70b71cce7
Leeseojeong <sjung0314@gmail.com>
2026-04-21 18:28:59 +0900

    ♻️ Refactor: home-category 아이콘 폴더 분리

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,7 +1,6 @@
 import ActivityIcon from './ic-activity.svg';
 import ArrowLeftIcon from './ic-arrow-left.svg';
 import ArrowRightIcon from './ic-arrow-right.svg';
-import ArtIcon from './ic-art.svg';
 import BackIcon from './ic-back.svg';
 import CaretDownIcon from './ic-caret-down.svg';
 import CaretLeftIcon from './ic-caret-left.svg';
@@ -12,7 +11,6 @@ import DeleteIcon from './ic-delete.svg';
 import EditIcon from './ic-edit.svg';
 import EyeOffIcon from './ic-eye-off.svg';
 import EyeOnIcon from './ic-eye-on.svg';
-import FoodIcon from './ic-food.svg';
 import GithubIcon from './ic-github.svg';
 import InfoIcon from './ic-info.svg';
 import KakaoIcon from './ic-kakao.svg';
@@ -26,16 +24,14 @@ import PlusIcon from './ic-plus.svg';
 import ReservationListIcon from './ic-reservation-list.svg';
 import ReservationStatusIcon from './ic-reservation-status.svg';
 import SearchIcon from './ic-search.svg';
-import SightseeingIcon from './ic-sightseeing.svg';
 import StarIcon from './ic-star.svg';
-import TourIcon from './ic-tour.svg';
-import WellbeingIcon from './ic-wellbeing.svg';
+
+export * from './home-category';
 
 export {
   ActivityIcon,
   ArrowLeftIcon,
   ArrowRightIcon,
-  ArtIcon,
   BackIcon,
   CaretDownIcon,
   CaretLeftIcon,
@@ -46,7 +42,6 @@ export {
   EditIcon,
   EyeOffIcon,
   EyeOnIcon,
-  FoodIcon,
   GithubIcon,
   InfoIcon,
   KakaoIcon,
@@ -60,8 +55,5 @@ export {
   ReservationListIcon,
   ReservationStatusIcon,
   SearchIcon,
-  SightseeingIcon,
   StarIcon,
-  TourIcon,
-  WellbeingIcon,
 };
