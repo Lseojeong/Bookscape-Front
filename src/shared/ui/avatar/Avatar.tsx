6e28f3aaff34cea4eddc1e6256fb4d5266f31d54
YEO Sugyeong <ssyeo1204@gmail.com>
2026-04-22 22:43:42 +0900

    ♻️ Refactor: 아바타 사이즈 상수명 camelCase로 변경

--- src/shared/ui/avatar/Avatar.tsx
+++ src/shared/ui/avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import { AVATAR_SIZE_CLASS_NAMES } from '@/shared/ui/avatar/AvatarConstants';
+import { avatarSizeClassNames } from '@/shared/ui/avatar/AvatarConstants';
 import AvatarFallback from '@/shared/ui/avatar/AvatarFallback';
 import AvatarImage from '@/shared/ui/avatar/AvatarImage';
 import { AvatarSizeProvider } from '@/shared/ui/avatar/context/avatarProvider';
@@ -17,7 +17,7 @@ const Avatar = (({ children, user, size = 'lg', className }: AvatarProps) => {
       <div
         className={cn(
           'relative inline-flex items-center justify-center overflow-hidden rounded-full',
-          AVATAR_SIZE_CLASS_NAMES[size],
+          avatarSizeClassNames[size],
           className
         )}
       >
--- src/shared/ui/avatar/AvatarConstants.ts
+++ src/shared/ui/avatar/AvatarConstants.ts
@@ -1,6 +1,6 @@
 import type { AvatarSize } from '@/shared/ui/avatar/types';
 
-export const AVATAR_SIZE_CLASS_NAMES: Record<AvatarSize, string> = {
+export const avatarSizeClassNames: Record<AvatarSize, string> = {
   sm: 'w-7.5 h-7.5', // 30px
   md: 'w-17.5 h-17.5 ', // 70px
   lg: 'w-30 h-30', // 120px
