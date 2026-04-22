11337269836dbc37b147bae4f07bbb72b84bc601
Leeseojeong <sjung0314@gmail.com>
2026-04-22 16:30:30 +0900

    🔧 Chore: 누락된 애드온 복구

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-onboarding', '@chromatic-com/storybook', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
