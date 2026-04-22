62f5f4a77ac55d58cd8a6724585b3bbfbd388fa6
daeun <ekdms6700@naver.com>
2026-04-23 05:39:23 +0900

    🐛 Fix: Pagination 화살표 버튼 비활성화 조건을 그룹 기준으로 수정

--- src/shared/ui/pagination/Pagination.tsx
+++ src/shared/ui/pagination/Pagination.tsx
@@ -93,8 +93,8 @@ export default function Pagination({
   const pageNumbers = getPageNumbers(currentPage, totalPages, pageGroupSize); // 현재 페이지 그룹에 해당하는 페이지 번호 배열
   const startPage = pageNumbers[0]; // 현재 페이지 그룹의 시작 페이지 번호
   const endPage = pageNumbers[pageNumbers.length - 1]; // 현재 페이지 그룹의 끝 페이지 번호
-  const isPrevDisabled = currentPage === 1; // 이전 페이지 버튼 비활성화 여부 (현재 페이지가 1이면 버튼 비활성화)
-  const isNextDisabled = currentPage === totalPages; // 다음 페이지 버튼 비활성화 여부 (현재 페이지가 마지막 페이지이면 버튼 비활성화)
+  const isPrevDisabled = startPage === 1; // 이전 페이지 버튼 비활성화 여부 (현재 페이지가 1이면 버튼 비활성화)
+  const isNextDisabled = endPage === totalPages; // 다음 페이지 버튼 비활성화 여부 (현재 페이지가 마지막 페이지이면 버튼 비활성화)
 
   const handlePrevClick = () => {
     if (!isPrevDisabled) {
