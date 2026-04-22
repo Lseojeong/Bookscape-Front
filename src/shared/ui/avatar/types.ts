ccca66892111b1c37c1f7aa03f919bd74ba662ed
YEO Sugyeong <ssyeo1204@gmail.com>
2026-04-23 00:33:03 +0900

    ♻️ Refactor: 미사용 타입 코드 삭제 및 AvatarComponent 타입 정의 수정

--- src/shared/ui/avatar/types.ts
+++ src/shared/ui/avatar/types.ts
@@ -2,16 +2,12 @@
  * avatar(프로필)에 사용되는 타입 정의
  */
 
-import { ReactNode } from 'react';
+import { FC } from 'react';
 import AvatarFallback from '@/shared/ui/avatar/AvatarFallback';
 import AvatarImage from '@/shared/ui/avatar/AvatarImage';
 
 /** Avatar 사이즈  */
 export type AvatarSize = 'sm' | 'md' | 'lg';
-
-export type AvatarImageProps = {
-  profileImageUrl?: string;
-};
 
 export type AvatarUser = {
   nickname?: string;
@@ -25,7 +21,7 @@ export type AvatarProps = {
   className?: string;
 };
 
-export type AvatarComponent = ((props: AvatarProps) => ReactNode) & {
+export type AvatarComponent = FC<AvatarProps> & {
   Img: typeof AvatarImage;
   Fallback: typeof AvatarFallback;
 };
