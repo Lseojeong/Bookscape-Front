e1083a5f0ce2cbec493941223f4b8af5f6c77ffb
hyewon <haeeon1123@gmail.com>
2026-04-23 17:20:53 +0900

    ✨ Feat: 마이페이지 사이드바 아바타 컴포넌트 연동

--- src/shared/ui/mypage-sidebar/MyPageSidebar.tsx
+++ src/shared/ui/mypage-sidebar/MyPageSidebar.tsx
@@ -1,9 +1,22 @@
+'use client';
+
+import Avatar from '@/shared/ui/avatar/Avatar';
+import type { AvatarProps } from '@/shared/ui/avatar/types';
 import MyPageNav from '@/shared/ui/mypage-sidebar/MyPageNav';
 
-export default function MyPageSidebar() {
+type MyPageSidebarProps = {
+  user: AvatarProps['user'];
+};
+
+export default function MyPageSidebar({ user }: MyPageSidebarProps) {
   return (
     <aside className="hidden max-w-44.5 rounded-xl border border-gray-50 bg-white px-3.5 py-6 shadow-[0_1px_10px_0_rgba(0,0,0,0.1)] min-[376px]:block lg:max-w-72.5">
-      <div className="mb-4">{}</div>
+      <div className="mb-4 flex justify-center lg:mb-6">
+        <Avatar user={user} size="md" className="lg:h-30 lg:w-30">
+          <Avatar.Img />
+          <Avatar.Fallback />
+        </Avatar>
+      </div>
       <MyPageNav />
     </aside>
   );
