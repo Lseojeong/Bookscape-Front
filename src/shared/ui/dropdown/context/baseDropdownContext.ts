294ac4c4a600fc2489b6c76acc607c6e9f026866
Leeseojeong <sjung0314@gmail.com>
2026-04-23 12:40:35 +0900

    ✨ Feat: base 드롭다운 context 추가

---
+++ src/shared/ui/dropdown/context/baseDropdownContext.ts
@@ -0,0 +1,19 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+
+/**
+ * Dropdown 컴포넌트 Context 타입
+ * @property isOpen - 드롭다운이 열려있는지 여부
+ * @property setIsOpen - 드롭다운 open 상태를 제어하는 함수
+ */
+type BaseDropdownContextType = {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+/**
+ * ## BaseDropdownContext
+ *
+ * @description
+ * Dropdown의 열림/닫힘(open) 상태만 담당하는 베이스 컨텍스트입니다.
+ */
+export const BaseDropdownContext = createContext<BaseDropdownContextType | undefined>(undefined);
