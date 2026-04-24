3e6bed0c3117bc525872812f733287064d07cf0f
hyewon <haeeon1123@gmail.com>
2026-04-25 00:27:35 +0900

    ✨ Feat: 마이페이지 공통 레이아웃 추가

---
+++ src/app/(private)/mypage/layout.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+import MyPageSidebar from '@/shared/ui/mypage-sidebar/MyPageSidebar';
+
+type MyPageLayoutProps = {
+  children: ReactNode;
+};
+
+// TODO: API 연결 후 실제 유저 데이터로 교체
+const MOCK_USER = {
+  nickname: '정만철',
+};
+
+export default async function MyPageLayout({ children }: MyPageLayoutProps) {
+  return (
+    <div className="mx-auto flex max-w-7xl justify-center gap-4 pt-3 sm:pt-10">
+      <MyPageSidebar user={MOCK_USER} />
+      <main>{children}</main>
+    </div>
+  );
+}
