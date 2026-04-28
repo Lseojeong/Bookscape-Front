6daba49e2c72ef4ca31481fa2ababee28d56f2f3
YEO Sugyeong <ssyeo1204@gmail.com>
2026-04-29 00:14:17 +0900

    🐛 Fix: 머지 충돌 해결

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,15 +1,7 @@
 import CardDefaultImage from './img-card-default.jpg';
 import DefaultProfileImage from './img-default-profile.svg?url';
-import EmptyImage from './img-empty.svg?url';
 import SkyImage from './img-sky.png';
 import SpeechBubbleImage from './img-speech-bubble.svg?url';
 import WarningImage from './img-warning.svg?url';
 
-export {
-  DefaultProfileImage,
-  EmptyImage,
-  SkyImage,
-  SpeechBubbleImage,
-  WarningImage,
-  CardDefaultImage,
-};
+export { DefaultProfileImage, SkyImage, SpeechBubbleImage, WarningImage, CardDefaultImage };
