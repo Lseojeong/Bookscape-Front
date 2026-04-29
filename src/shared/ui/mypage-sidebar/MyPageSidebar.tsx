f311070377ec7c3be22ba9c463ac479944e5e9a1
hyewon <haeeon1123@gmail.com>
2026-04-29 15:29:13 +0900

    🎨 Style: sm:top 값 수정

--- src/shared/ui/mypage-sidebar/MyPageSidebar.tsx
+++ src/shared/ui/mypage-sidebar/MyPageSidebar.tsx
@@ -32,7 +32,7 @@ type MyPageSidebarProps = {
 export default function MyPageSidebar({ user }: MyPageSidebarProps) {
   return (
     <>
-      <aside className="sticky hidden h-fit w-44.5 shrink-0 rounded-xl border border-gray-50 bg-white px-3.5 py-4 shadow-drop sm:top-12 sm:block md:top-30 lg:w-72.5 lg:py-6">
+      <aside className="sticky hidden h-fit w-44.5 shrink-0 rounded-xl border border-gray-50 bg-white px-3.5 py-4 shadow-drop sm:top-22 sm:block md:top-30 lg:w-72.5 lg:py-6">
         <div className="mb-3 flex justify-center lg:mb-6">
           <Avatar user={user} size="md" className="lg:h-30 lg:w-30">
             <Avatar.Img />
