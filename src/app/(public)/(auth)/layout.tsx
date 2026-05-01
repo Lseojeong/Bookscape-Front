a62be962c0b5471bd9268d555c88a4206d2768a6
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-01 17:30:59 +0900

    🎨 Style: 인증 레이아웃 최적화 및 브랜딩 영역 UI 버그 수정

--- src/app/(public)/(auth)/layout.tsx
+++ src/app/(public)/(auth)/layout.tsx
@@ -1,5 +1,8 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import AuthBranding from '@/features/auth/ui/AuthBranding';
 import AuthHeadline from '@/features/auth/ui/AuthHeadline';
+import { cn } from '@/shared/utils/cn';
 
 /**
  * 로그인 및 회원가입 페이지에서 공통으로 사용되는 레이아웃 컴포넌트입니다.
@@ -11,13 +14,21 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isSignupPage = pathname.includes('signup');
+
   return (
-    <main className="flex justify-between">
-      <div className="mt-13.5 mb-29.5 grow px-6 md:mt-29.5 md:px-13.5">
+    <main className="flex min-h-dvh w-full items-center justify-center lg:justify-between">
+      <div className={cn('mx-auto w-full max-w-187 px-6 md:px-13.5', isSignupPage && 'py-20')}>
         <AuthHeadline />
         <div className="mt-17 md:mt-22">{children}</div>
       </div>
-      <div className="hidden shrink-0 transition-all lg:block lg:w-130 xl:w-200 2xl:w-250">
+      <div
+        className={cn(
+          'hidden shrink-0 transition-all xl:block',
+          'xl:min-h-dvh xl:w-200 xl:self-stretch 2xl:w-250'
+        )}
+      >
         <AuthBranding />
       </div>
     </main>
