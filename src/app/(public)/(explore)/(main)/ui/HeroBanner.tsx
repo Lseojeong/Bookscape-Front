2b2a792fc9d9b30876baa905842bf610aecc7caf
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-03 23:09:33 +0900

    🎨 Style: 버튼 호버 스타일 추가 및 검색 인풋 위치 최적화

--- src/app/(public)/(explore)/(main)/ui/HeroBanner.tsx
+++ src/app/(public)/(explore)/(main)/ui/HeroBanner.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 import { HeroBannerImage } from '@/shared/assets/images';
 import Logo from '@/shared/ui/logo/Logo';
 import SearchInput from '@/shared/ui/search-input/SearchInput';
+import { cn } from '@/shared/utils/cn';
 
 export default function HeroBanner(props: Omit<ImageProps, 'src' | 'alt'>) {
   return (
-    <section className="relative h-105">
+    <section className="relative h-105 w-full overflow-visible">
       {/* 배경 이미지 */}
       <Image
         src={HeroBannerImage}
@@ -19,7 +20,7 @@ export default function HeroBanner(props: Omit<ImageProps, 'src' | 'alt'>) {
       {/* 헤드라인 및 검색 인풋 */}
       <div className="absolute inset-0 mx-auto mt-22 flex max-w-260 flex-col items-center justify-between text-white md:mt-13.25 lg:mt-20.75">
         <div className="flex w-full items-center justify-between">
-          <div className="grow">
+          <div className="max-w-2xl grow">
             <p className="mb-10.25 typo-16-medium md:mb-6.25">Search</p>
             <p className="mb-10.25 typo-20-medium md:mb-8.75 lg:mb-13.25">
               체험을 만들고, 찾고, 예약까지 한 번에
@@ -29,14 +30,17 @@ export default function HeroBanner(props: Omit<ImageProps, 'src' | 'alt'>) {
             <Link
               href="/activities"
               aria-label="체험 페이지로 이동"
-              className="inline-block w-full rounded-4xl bg-white py-4 text-center typo-13-medium text-primary-700 md:w-86.5"
+              className={cn(
+                'inline-block w-full rounded-4xl bg-white py-4 text-center typo-13-medium text-primary-700 md:w-86.5',
+                'transition-transform hover:scale-105'
+              )}
             >
               지금 바로 체험 보러 가기
             </Link>
           </div>
-          <Logo variant="white" className="hidden w-63 lg:block" />
+          <Logo variant="white" className="hidden shrink-0 lg:block lg:w-60" />
         </div>
-        <div className="w-full translate-y-8">
+        <div className="w-full translate-y-1/2">
           <SearchInput />
         </div>
       </div>
