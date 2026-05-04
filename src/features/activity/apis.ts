c043fa4a2c464e6a908d6a0f0c8e2abb3402bcc9
daeun <ekdms6700@naver.com>
2026-05-05 00:10:32 +0900

    📝 Docs: 체험 상세 API 주석 추가

--- src/features/activity/apis.ts
+++ src/features/activity/apis.ts
@@ -5,6 +5,7 @@ export const getActivityDetail = async (id: number) => {
   const data = await get(`/activities/${id}`);
   const activity = ActivityDetailSchema.parse(data);
 
+  // NOTE: bannerImageUrl, subImages를 순서대로 합쳐서 반환
   const images = [activity.bannerImageUrl, ...activity.subImages.map((img) => img.imageUrl)];
 
   return { ...activity, images };
