f3092354ed1036bc20cc7b997a781005b09133d6
daeun <ekdms6700@naver.com>
2026-05-05 00:18:02 +0900

    ♻️ Refactor: 이미지 캐러셀 스토리 수정

--- src/features/activity/activity-detail/ui/ImageCarousel.stories.tsx
+++ src/features/activity/activity-detail/ui/ImageCarousel.stories.tsx
@@ -11,6 +11,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    images: [
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/22-1_3417_1777887801764.jpeg',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/22-1_3417_1777887831214.jpeg',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/22-1_3417_1777888188557.jpeg',
+    ],
+  },
   decorators: [
     (Story) => (
       <div className="w-167.5">
