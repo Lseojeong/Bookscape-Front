03707a56119c7adf192ddbf854ef8529dcdf7230
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-05 19:15:08 +0900

    ✨ Feat: 검색 시 검색페이지로 이동 반영

--- src/shared/ui/search-input/SearchInput.tsx
+++ src/shared/ui/search-input/SearchInput.tsx
@@ -34,7 +34,7 @@ export default function SearchInput() {
     }
 
     // 변경된 query로 URL 이동 (검색 상태 반영)
-    router.replace(`?${params.toString()}`);
+    router.replace(`search/?${params.toString()}`);
   };
 
   return <SearchInputUi onSubmit={handleSubmit} defaultValue={keyword} />;
