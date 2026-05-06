10b81ec95f72226353592d80d51d626448740131
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-06 21:21:41 +0900

    ♻️ Refactor: 사용안하는 div 태그 <>로 변경

--- src/app/(public)/(explore)/(main)/page.tsx
+++ src/app/(public)/(explore)/(main)/page.tsx
@@ -4,12 +4,12 @@ import MainActivityList from '@/features/activity/main/ui/MainActivityList';
 
 export default function MainPage() {
   return (
-    <div>
+    <>
       <HeroBanner />
       <div className="mx-auto mt-21.5 mb-10 flex max-w-280 flex-col gap-13.25 px-6 md:mt-37.5 md:mb-25 md:gap-28.75 md:px-7.5 xl:px-0">
         <MainCategoryList />
         <MainActivityList />
       </div>
-    </div>
+    </>
   );
 }
