cdd6823ca7ffe9d613b9ec94466dc4e925e87b43
daeun <ekdms6700@naver.com>
2026-05-06 15:50:11 +0900

    ♻️ Refactor: ActivityInfo 스토리 수정

--- src/features/activity/activity-detail/ui/ActivityInfo.stories.tsx
+++ src/features/activity/activity-detail/ui/ActivityInfo.stories.tsx
@@ -11,6 +11,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
   args: {
     id: 1,
     category: '문화 · 예술',
