c3a9d6ae2d1feaf5fcadb760edfa264850bbe07e
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-06 20:38:28 +0900

    ✨ Feat: 인기 체험 목록 없을 때 조건부 empty state 컴포넌트 렌더링

--- src/features/activity/main/ui/MainActivityList.tsx
+++ src/features/activity/main/ui/MainActivityList.tsx
@@ -4,6 +4,7 @@ import useMainActivityCarousel from '@/features/activity/main/hooks/useMainActiv
 import ActivityCard from '@/features/activity/main/ui/activity-card/ActivityCard';
 import CarouselArrowButton from '@/features/activity/main/ui/CarouselArrowButton';
 import { ArrowLeftIcon, ArrowRightIcon } from '@/shared/assets/icons';
+import EmptyState from '@/shared/ui/empty-state/EmptyState';
 import Title from '@/shared/ui/title/Title';
 
 /**
@@ -24,45 +25,53 @@ export default function MainActivityList() {
       <Title as="h2" size="18" weight="bold" className="text-gray-950 md:typo-32-bold">
         🔥 <span className="text-secondary-500">HOT</span> 인기 체험
       </Title>
-      <div className="group relative">
-        {/* 왼쪽 버튼 */}
-        <CarouselArrowButton
-          className="-left-5"
-          onClick={scrollPrev}
-          disabled={!canScrollPrev}
-          ariaLabel="이전"
-        >
-          <ArrowLeftIcon />
-        </CarouselArrowButton>
 
-        {/* 이미지 캐러셀 */}
-        <div ref={emblaRef} className="overflow-hidden pb-4">
-          <ul className="flex gap-4 md:gap-5 lg:gap-6">
-            {activityData.map((data) => {
-              return (
-                <li
-                  key={data.id}
-                  className="w-[calc((100%-50px*2)/2)] min-w-50 flex-none md:w-[calc((100%-20px*1)/2)] lg:w-[calc((100%-24px*3)/4)]"
-                >
-                  <Link href={`/activity/${data.id}`}>
-                    <ActivityCard data={data} />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+      {activityData.length === 0 ? (
+        <EmptyState
+          type="experience"
+          mainText={'아직 인기 체험이 없어요.\n첫 번째 후기의 주인공이 되어보시는 건 어떨까요?'}
+        />
+      ) : (
+        <div className="group relative">
+          {/* 왼쪽 버튼 */}
+          <CarouselArrowButton
+            className="-left-5"
+            onClick={scrollPrev}
+            disabled={!canScrollPrev}
+            ariaLabel="이전"
+          >
+            <ArrowLeftIcon />
+          </CarouselArrowButton>
+
+          {/* 이미지 캐러셀 */}
+          <div ref={emblaRef} className="overflow-hidden pb-4">
+            <ul className="flex gap-4 md:gap-5 lg:gap-6">
+              {activityData.map((data) => {
+                return (
+                  <li
+                    key={data.id}
+                    className="w-[calc((100%-50px*2)/2)] min-w-50 flex-none md:w-[calc((100%-20px*1)/2)] lg:w-[calc((100%-24px*3)/4)]"
+                  >
+                    <Link href={`/activity/${data.id}`}>
+                      <ActivityCard data={data} />
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          {/* 오른쪽 버튼 */}
+          <CarouselArrowButton
+            onClick={scrollNext}
+            disabled={!canScrollNext}
+            className="-right-5"
+            ariaLabel="다음"
+          >
+            <ArrowRightIcon />
+          </CarouselArrowButton>
         </div>
-
-        {/* 오른쪽 버튼 */}
-        <CarouselArrowButton
-          onClick={scrollNext}
-          disabled={!canScrollNext}
-          className="-right-5"
-          ariaLabel="다음"
-        >
-          <ArrowRightIcon />
-        </CarouselArrowButton>
-      </div>
+      )}
     </section>
   );
 }
