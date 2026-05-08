4b649281e1456261efe1fbbee4f0055f2249ae15
Leeseojeong <sjung0314@gmail.com>
2026-05-09 03:30:47 +0900

    ♻️ Refactor: zod 타입으로 변경 후 런타임 검증 적용

--- src/features/user/types.ts
+++ src/features/user/types.ts
@@ -1,18 +1,26 @@
-export type UserResponse = {
-  id: number;
-  email: string;
-  nickname: string;
-  profileImageUrl: string | null;
-  createdAt: string;
-  updatedAt: string;
-};
+import { z } from 'zod';
 
-export type UpdateMyProfileRequestBody = {
-  nickname?: string;
-  profileImageUrl?: string;
-  newPassword?: string;
-};
+export const UserResponseSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  nickname: z.string(),
+  profileImageUrl: z.string().nullable(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
 
-export type CreateMyProfileImageUrlResponse = {
-  profileImageUrl: string;
-};
+export type UserResponse = z.infer<typeof UserResponseSchema>;
+
+export const UpdateMyProfileRequestBodySchema = z.object({
+  nickname: z.string().optional(),
+  profileImageUrl: z.string().optional(),
+  newPassword: z.string().optional(),
+});
+
+export type UpdateMyProfileRequestBody = z.infer<typeof UpdateMyProfileRequestBodySchema>;
+
+export const CreateMyProfileImageUrlResponseSchema = z.object({
+  profileImageUrl: z.string(),
+});
+
+export type CreateMyProfileImageUrlResponse = z.infer<typeof CreateMyProfileImageUrlResponseSchema>;
