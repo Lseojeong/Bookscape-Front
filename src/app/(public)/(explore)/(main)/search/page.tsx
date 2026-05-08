46c8d3eee3c4e750a22e3367580a2876f45cdf4f
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-08 17:29:40 +0900

    🐛 Fix: 검색 결과 컴포넌트 Suspense로 감싸기

--- src/app/(public)/(explore)/(main)/search/page.tsx
+++ src/app/(public)/(explore)/(main)/search/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import HeroBanner from '@/app/(public)/(explore)/(main)/ui/HeroBanner';
 import MainSectionLayout from '@/app/(public)/(explore)/(main)/ui/MainSectionLayout';
 import SearchResultSection from '@/app/(public)/(explore)/(main)/ui/SearchResultSection';
@@ -13,7 +14,9 @@ export default function SearchPage() {
       <HeroBanner />
       {/* 검색 결과 */}
       <MainSectionLayout className="md:mt-28.75">
-        <SearchResultSection />
+        <Suspense fallback={<div>...</div>}>
+          <SearchResultSection />
+        </Suspense>
       </MainSectionLayout>
     </>
   );
