d7589727f34073cab29397c82d145170d47ff358
Leeseojeong <sjung0314@gmail.com>
2026-05-10 17:33:54 +0900

    ♻️ Refactor: query use client 제거

--- src/features/my-page/my-activity/queries/useMyActivities.ts
+++ src/features/my-page/my-activity/queries/useMyActivities.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import { useQuery } from '@tanstack/react-query';
 import { getMyActivities } from '@/features/my-page/my-activity/apis';
 import type { GetMyActivitiesQuery } from '@/features/my-page/types';
