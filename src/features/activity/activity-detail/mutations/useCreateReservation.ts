62e45913accec425e72dd9b4ed27a580017d4c48
Leeseojeong <sjung0314@gmail.com>
2026-05-13 01:14:29 +0900

    🔥 Remove: 사용하지 않는 activity-panel 삭제

--- src/features/activity/activity-detail/mutations/useCreateReservation.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { createReservation } from '@/features/activity/apis';
-
-export const useCreateReservation = (activityId: number) => {
-  return useMutation({
-    mutationFn: ({ scheduleId, headCount }: { scheduleId: number; headCount: number }) =>
-      createReservation(activityId, { scheduleId, headCount }),
-  });
-};
--- src/features/my-page/common/useDeleteActivity.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { deleteActivity } from '@/features/activity/apis';
-
-export const useDeleteActivity = () => {
-  return useMutation({
-    mutationFn: (id: number) => deleteActivity(id),
-  });
-};
