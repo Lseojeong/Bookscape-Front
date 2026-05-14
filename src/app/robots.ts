ce001816ac7930e0c23a753c39bab3bacca2db5b
bitna0a <qlcsk0147@naver.com>
2026-05-14 16:24:07 +0900

    ✨ Feat: 검색 차단 페이지 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*', // 모든 검색 엔진 봇 접근 허용
       allow: '/', // 사이트 전체 페이지 수집
+      disallow: ['/mypage/', '/activity/*/edit', '/activity/new'],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
