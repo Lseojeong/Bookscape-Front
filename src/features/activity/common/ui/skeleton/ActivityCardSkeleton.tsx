fe7dd4c66c6aeb29fe42e3c892d5394be265b9af
YEO Sugyeong <ssyeo1204@gmail.com>
2026-05-14 22:04:03 +0900

    🎨 Style: 체험 카드 스타일 변경

--- src/features/activity/common/ui/skeleton/ActivityCardSkeleton.tsx
+++ src/features/activity/common/ui/skeleton/ActivityCardSkeleton.tsx
@@ -1,3 +1,5 @@
+import BaseCardInfo from '@/shared/ui/card/base/BaseCardInfo';
+import { cardWrapStyles } from '@/shared/ui/card/cardStyles';
 import Skeleton from '@/shared/ui/skeleton/Skeleton';
 import { cn } from '@/shared/utils/cn';
 
@@ -11,17 +13,17 @@ import { cn } from '@/shared/utils/cn';
  */
 export default function ActivityCardSkeleton({ className }: { className?: string }) {
   return (
-    <div className={cn('rounded-[18px] border border-gray-100 p-4 md:rounded-4xl', className)}>
-      {/* 이미지 영역 */}
-      <Skeleton className="h-30 w-full rounded-xl md:h-75 lg:h-56" />
-      <div className="mt-3 mb-4 flex w-full flex-col gap-2 md:mt-3 md:mb-5">
-        {/* 제목 */}
-        <Skeleton className="h-4 w-4/5 rounded-xl md:h-5 md:w-2/3" />
-        {/* 리뷰 */}
-        <Skeleton className="h-4 w-16 rounded-xl md:h-5 md:w-1/4" />
-      </div>
-      {/* 가격 */}
-      <Skeleton className="h-4 w-20 rounded-xl md:h-5 md:w-2/5" />
+    <div className={cn(cardWrapStyles, className)}>
+      <Skeleton className="relative mb-16.5 h-44 w-full overflow-hidden rounded-xl md:mb-19 md:h-93.5 lg:h-72.5" />
+      <BaseCardInfo className="absolute bottom-0 p-4 md:px-7.5 md:py-5">
+        <div className="flex flex-col gap-2.5 md:gap-4.5">
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-4 w-4/5 rounded-xl md:h-5 md:w-2/3 lg:w-40" />
+            <Skeleton className="h-4 w-16 rounded-xl md:h-5 md:w-1/4" />
+          </div>
+          <Skeleton className="h-4 w-20 rounded-xl md:h-5 md:w-2/5" />
+        </div>
+      </BaseCardInfo>
     </div>
   );
 }
