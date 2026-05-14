dbaa3f8e2d42b55c3d8b4239b9d3d11a438bdcab
Leeseojeong <sjung0314@gmail.com>
2026-05-15 04:49:39 +0900

    ♻️ Refactor: 예약 내역/체험 관리 page-header 서버 컴포넌트로 이동

--- src/app/(private)/mypage/activity/page.tsx
+++ src/app/(private)/mypage/activity/page.tsx
@@ -1,13 +1,16 @@
 import MyActivityCreateButton from '@/features/my-page/my-activity/ui/MyActivityCreateButton';
 import MyActivityList from '@/features/my-page/my-activity/ui/MyActivityList';
-import MyActivityPageHeader from '@/features/my-page/my-activity/ui/MyActivityPageHeader';
+import PageHeader from '@/shared/ui/page-header/PageHeader';
 
 export default function MyActivityPage() {
   return (
     <div className="mb-10 flex w-full flex-col gap-4 md:w-119 md:gap-7.5 lg:w-160">
       {/* Page Header */}
       <div className="flex items-start justify-between">
-        <MyActivityPageHeader />
+        <PageHeader
+          title="내 체험 관리"
+          description="체험을 등록하거나 수정 및 삭제가 가능합니다."
+        />
         <MyActivityCreateButton />
       </div>
 
