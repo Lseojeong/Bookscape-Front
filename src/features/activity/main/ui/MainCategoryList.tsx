a2d1b1b4fd297cfbd76d167211bb9a1acf37c8d5
daeun <ekdms6700@naver.com>
2026-05-18 17:52:16 +0900

    🎨 Style: 전체보기 아이콘 크기 명시

--- src/features/activity/main/ui/MainCategoryList.tsx
+++ src/features/activity/main/ui/MainCategoryList.tsx
@@ -39,8 +39,8 @@ export default function MainCategoryList() {
           className="inline-flex items-center typo-16-medium text-primary-700 md:typo-20-medium"
         >
           전체 보기
-          <span className="flex h-4 w-4 items-center justify-center md:h-6 md:w-6">
-            <ChevronRightBlueIcon />
+          <span className="flex items-center justify-center">
+            <ChevronRightBlueIcon className="h-4 w-4 md:h-6 md:w-6" />
           </span>
         </Link>
       </div>
